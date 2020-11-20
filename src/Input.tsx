7a7e2d7453f7c36cb2a715ac5d9f0c166ecfb06c
Jesse Gibson <JesseTheGibson@gmail.com>
2020-11-20 00:50:00 -0800

    Implement hybrid controllable input
    This input implements both controlled and uncontrolled behavior using
    the Hybrid Controllable Input pattern. If you haven't heard of it, then
    perhaps I never got around to writing the blog post.
    
    Here's the gist: inputs can behave as both controllable and uncontrolled
    by dynamically switching between props and state depending on element
    focus. This enables rich freeform editing for the user while maintaining
    all the benefits of controllable inputs (plus better React performance).
    
    This input doesn't support all input types, nor should it. Radios,
    select boxes, checkmarks, etc., they don't benefit from the kind of
    optimizations this library tries to implement.

--- src/Input.tsx
+++ src/Input.tsx
@@ -1,11 +1,69 @@
-import React, { FC, InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes } from 'react';
 
-interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  // TODO
+const noop = () => {};
+
+export default class FreeformInput extends React.Component<Props, State> {
+  static defaultProps = {
+    onFocus: noop,
+    onBlur: noop,
+    onChange: noop,
+    onInput: noop,
+  };
+
+  state = {
+    value: '',
+    mode: Mode.Dormant,
+  };
+
+  render() {
+    const { value, onChange, ...props } = this.props;
+
+    return (
+      <input
+        {...props}
+        onFocus={this.enterEditMode}
+        onInput={this.updateLocalSnapshot}
+        onBlur={this.exitEditMode}
+        value={this.state.mode === Mode.Active ? this.state.value : value}
+      />
+    );
+  }
+
+  enterEditMode = (event: React.FocusEvent<HTMLInputElement>) => {
+    const snapshot =
+      typeof this.props.value !== 'undefined' ? this.props.value : '';
+
+    this.setState({ mode: Mode.Active, value: snapshot });
+    this.props.onFocus!(event);
+  };
+
+  updateLocalSnapshot = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ value: event.currentTarget.value });
+    this.props.onInput!(event);
+  };
+
+  exitEditMode = (event: React.FocusEvent<HTMLInputElement>) => {
+    this.setState({ mode: Mode.Dormant });
+
+    if (this.state.value !== this.props.value) {
+      this.props.onChange!(this.state.value);
+    }
+
+    this.props.onBlur!(event);
+  };
 }
 
-const Input: FC<Props> = () => {
-  return <input />;
-};
+interface Props
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
+  onChange?: (value: string) => void;
+}
 
-export default Input;
+interface State {
+  value: Props['value'];
+  mode: Mode;
+}
+
+enum Mode {
+  Active,
+  Dormant,
+}
--- src/__tests__/Input.test.tsx
+++ src/__tests__/Input.test.tsx
@@ -6,18 +6,115 @@ import Input from '../Input';
 describe('Input', () => {
   function setup<Props>(overrides?: Props) {
     const props = {
+      value: 'controlled-value',
+      onChange: jest.fn(),
+      onFocus: jest.fn(),
+      onInput: jest.fn(),
+      onBlur: jest.fn(),
       ...overrides,
     };
 
     const output = shallow(<Input {...props} />);
 
+    const simulate = {
+      focus: () => output.simulate('focus'),
+      blur: () => output.simulate('blur'),
+      input: (value: string) =>
+        output.simulate('input', {
+          currentTarget: { value },
+        }),
+    };
+
     return {
       output,
       props,
+      simulate,
     };
   }
 
-  it('renders', () => {
-    expect(setup).not.toThrow();
+  it('defaults to controlled mode', () => {
+    const { output, props } = setup();
+
+    expect(output.prop('value')).toBe(props.value);
+  });
+
+  it('blindly forwards all input props', () => {
+    const { output } = setup({ random: 'prop', extraneous: true });
+
+    expect(output.prop('random')).toBe('prop');
+    expect(output.prop('extraneous')).toBe(true);
+  });
+
+  it('emits change events', () => {
+    const { simulate, props } = setup();
+
+    simulate.focus();
+    simulate.input('new conte');
+    simulate.input('new conten');
+    simulate.input('new content');
+    simulate.blur();
+
+    expect(props.onChange).toHaveBeenCalledTimes(1);
+    expect(props.onChange).toHaveBeenCalledWith('new content');
+  });
+
+  it('emits native events', () => {
+    const { simulate, props } = setup();
+
+    simulate.focus();
+    expect(props.onFocus).toHaveBeenCalled();
+
+    simulate.input('content');
+    expect(props.onInput).toHaveBeenCalled();
+
+    simulate.blur();
+    expect(props.onBlur).toHaveBeenCalled();
+  });
+
+  it('survives when events are omitted', () => {
+    const { simulate } = setup({
+      onFocus: undefined,
+      onBlur: undefined,
+      onInput: undefined,
+      onChange: undefined,
+    });
+
+    const events = () => {
+      simulate.focus();
+      simulate.input('bacon');
+      simulate.blur();
+    };
+
+    expect(events).not.toThrow();
+  });
+
+  it('ignores changes if the new input matches props', () => {
+    const { simulate, props } = setup();
+
+    simulate.focus();
+    simulate.input(props.value);
+    simulate.blur();
+
+    expect(props.onChange).not.toHaveBeenCalled();
+  });
+
+  it('uses state as the controlled value in edit mode', () => {
+    const { simulate, output } = setup();
+
+    simulate.focus();
+    simulate.input('new content');
+
+    expect(output.prop('value')).toBe('new content');
+  });
+
+  it('forks prop state on edit mode and reverts afterward', () => {
+    const { simulate, output, props } = setup();
+
+    simulate.focus();
+    expect(output.prop('value')).toBe(props.value);
+
+    simulate.input('new content');
+    simulate.blur();
+    expect(output.prop('value')).toBe(props.value);
   });
 });
